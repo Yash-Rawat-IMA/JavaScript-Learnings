d1e3253e2ddb141beb142cd2579f28897e743261
Yash-Rawat-IMA <yash97181004@gmail.com>
2025-07-02 14:37:48 +0530

    Conditions: if-else to understand how code works in Programming Language

---
+++ conditions/conditions_challanges.js
@@ -0,0 +1,40 @@
+let u1="Yash"
+let authuser="Yash"
+
+if(u1==authuser)
+{
+    console.log("User aloowed");
+}
+else{
+    console.log(
+        "User not allowed"
+    );
+}
+
+let bal=45;
+if(typeof bal === "number")
+{
+    console.log("Yes it's a number");
+}
+else{
+    console.log("No, it's not a number");
+}
+
+let isAct=true;
+if(isAct)
+{
+    console.log("User is Active");
+}
+else{
+    console.log("User is not active");
+}
+
+let arr=[];
+console.log(arr.length);
+if(arr.length==0)
+{
+    console.log("Array is empty");
+}
+else{
+    console.log("Array is not empty");
+}
